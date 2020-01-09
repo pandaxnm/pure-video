40b60408f5045d6120a0277fbfb81ae934d6d84e
pandaxnm <pandaxnm@163.com>
2020-01-09 13:45:25 +0800

    update

---
+++ web/src/utils/api.js
@@ -0,0 +1,12 @@
+const api = {
+    home: '/video/index',
+    detail: '/video/detail',
+    playInfo: '/video/play-info',
+    banner: '/video/banner',
+    categories: '/video/categories',
+    hot: '/video/hot',
+    search: '/video/search',
+
+};
+
+export default api;
